94937e866087fe293d1029d4c58c050b0c2d7298
makura3 <37650810+makura3@users.noreply.github.com>
2018-06-04 19:31:17 +0900

    add index pages.

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      ヘッダー
+    </header>
+
+    <nuxt></nuxt>
+    
+    <footer>
+      フッター
+    </footer>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ text }} world!</p>
+    <p>テンプレートの部分は別ファイルに切り出すことが可能です。</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'hello'
+      }
+    }
+  }
+</script>
