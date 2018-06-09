be2d9f0c26700c7544734e51dd141593a2c47eb2
makura3 <37650810+makura3@users.noreply.github.com>
2018-06-09 20:13:21 +0900

    Add section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,25 @@
   <div>
     <WideImage />
     <defaultWhitePanel />
+    <section class="bg-fast-yellow no-padding wow fadeInUp animated">
+      <div class="container">
+        <div class="row padding-five sm-text-center">
+          <div class="col-md-1">
+            <i class="medium-icon black-text no-margin icon-happy"></i>
+          </div>
+          <div class="col-md-6 no-padding">
+            <span class="text-med text-uppercase letter-spacing-2 margin-two black-text font-weight-600 xs-margin-top-six xs-margin-bottom-six display-block">
+            JOIN US! 業務拡大に付き、クリエイター絶賛大募集 </span>
+          </div>
+          <div class="col-md-5 no-padding">
+            <a class="highlight-button-dark btn btn-medium button xs-margin-bottom-five xs-no-margin-right" href="https://spice-factory.co.jp/member/">
+            働くメンバーを見てみる </a>
+            <a class="highlight-button btn btn-medium button xs-margin-bottom-five xs-no-margin-right" href="https://spice-factory.co.jp/recruit/">
+            採用情報を見てみる </a>
+          </div>
+        </div>
+      </div>
+    </section>
     <p>{{ text }} world!</p>
     <p>テンプレートの部分は別ファイルに切り出すことが可能です。</p>
   </div>
