35d5b4e22556209d98360dc4b7c849abdccfd3c6
makura3 <37650810+makura3@users.noreply.github.com>
2018-06-13 16:35:11 +0900

    add firebase setting file.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <section class="item-wrap">
+      <!-- <Item />
       <Item />
-      <Item />
-      <Item />
+      <Item /> -->
+      <p v-for="(item, key, index) in items" :key="index">
+        {{ item.data().name }}
+        {{ item.data().description }}
+      </p>
     </section>
     <p>{{ text }} world!</p>
     <p>テンプレートの部分は別ファイルに切り出すことが可能です。</p>
@@ -16,6 +20,21 @@ import Item from '~/components/panel/Item.vue'
 export default {
   components: {
     Item
+  },
+  async asyncData({ app }) {
+    try {
+      await app.$firebase.firestore().enablePersistence()
+      const items = await app.$firebase
+        .firestore()
+        .collection('items')
+        .limit(5)
+        .get()
+      return {
+        items: items.docs
+      }
+    } catch (err) {
+      console.log(err)
+    }
   },
   data() {
     return { text: 'hellow' }
